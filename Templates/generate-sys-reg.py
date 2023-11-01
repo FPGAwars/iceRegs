214a6ddd747ed11dbe27fd60faae01cc2224ee90
Obijuan <obijuan.cube@gmail.com>
2023-11-01 11:11:15 +0100

    update generation script: block id is used for the target folder

--- Templates/generate-sys-reg.py
+++ Templates/generate-sys-reg.py
@@ -1,17 +1,16 @@
 from icestudio import generate_block_from_template
 
-
-#-- Icestudio block: Sys-reg
+BLOCK_ID = "Sys-reg"
 VERSION = "0.8"
-TARGET_PATH = "../blocks/Sys-Regs"
+TARGET_PATH = f"../blocks/{BLOCK_ID}"
 DESCRIPTION = "system register. Verilog implementation"
 
 #-- Debug: Generate only one block in the current path for testing
-#generate_block_from_template(3,"Sys-reg", VERSION, DESCRIPTION)
+#generate_block_from_template(3,BLOCK_ID, VERSION, DESCRIPTION)
 
 #-- Generate all the blocks of different sizes (from 2 to 32 bits)
 for i in range(2,33):
-    generate_block_from_template(i, "Sys-reg", VERSION, 
+    generate_block_from_template(i, BLOCK_ID, VERSION, 
                                  DESCRIPTION,TARGET_PATH)  
 
 
